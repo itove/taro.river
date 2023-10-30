05f2b9a4d557879419152e042e9fb81820a2c4fe
Al Zee <z@alz.ee>
2023-10-30 16:24:52 +0800

    configs

--- config/index.ts
+++ config/index.ts
@@ -9,7 +9,7 @@ const config = {
     375: 2 / 1
   },
   sourceRoot: 'src',
-  outputRoot: 'dist',
+  outputRoot: '../../../../mnt/d/subao',
   plugins: ['@tarojs/plugin-html'],
   defineConstants: {
   },
