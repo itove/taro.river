f82d60db24fdd76d940e11d6ef3574365358fe19
Al Zee <z@alz.ee>
2023-10-30 16:30:09 +0800

    app.config.ts

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,34 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/node/index',
+    'pages/node/show',
+    'pages/node/edit',
+    'pages/me/index',
+    'pages/me/setting'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    // custom: true,
+    selectedColor: '#000000',
+    list: [
+      {
+        "pagePath": "pages/index/index",
+        "text": "首页",
+        "iconPath": './icon/home.png',
+        "selectedIconPath": './icon/home-fill.png'
+      },
+      {
+        "pagePath": "pages/me/index",
+        "text": "我的",
+        "iconPath": './icon/account.png',
+        "selectedIconPath": './icon/account-fill.png'
+      },
+    ]
   }
 })
