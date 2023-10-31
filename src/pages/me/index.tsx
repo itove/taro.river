e9f88b9f3dec3271f986ee4ed231170fa6d1e72a
Al Zee <z@alz.ee>
2023-10-31 16:53:00 +0800

    nth

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,19 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View } from '@tarojs/components'
-import { Button } from "@nutui/nutui-react-taro"
+import { Button, Cell } from "@nutui/nutui-react-taro"
+import { Right } from '@nutui/icons-react-taro'
 import './index.scss'
+import Taro from '@tarojs/taro'
+import { Env } from '../../env'
+
+// const attrs = {
+//   name: 'jess',
+//   phone: '1329191',
+//   phone1: 'df29191'
+// }
+
+function goto() {
+  const link = '/pages/node/index'
+  Taro.navigateTo({ url: link })
+}
 
 function Index() {
+  // const [list, setList] = useState([])
+  const [user, setUser] = useState({})
+  let l = []
+  let uid: int
+
+  useEffect(() => {
+    Taro.getStorage({
+      key: Env.storageKey
+    })
+    .then(res => {
+      uid = res.data.id
+
+      // fetch data
+      Taro.request({
+        url: Env.apiUrl + 'users/' + res.data.id
+      })
+      .then(res => {
+        console.log(res)
+        setUser(res.data)
+      })
+
+      // for (let i in attrs) {
+      // const extra= <><span>{attrs[i]}</span><Right /></>
+      // l.push(
+      //     <Cell
+      //     className='nutui-cell--clickable'
+      //     title={i}
+      //     align='center'
+      //     extra={extra}
+      //     onClick={goto}
+      //     />
+      //     )
+      // }
+      // console.log(l)
+      // setList(l)
+    })
+
+  }, [])
+
   return (
     <View className="nutui-react-demo">
-      <View className="index">
-        欢迎使用 NutUI React 开发 Taro 多端项目。
-      </View>
-      <View className="index">
-        <Button type="primary" className="btn">
-          NutUI React Button
-        </Button>
-      </View>
+      <Cell.Group>
+        <Cell
+        className='nutui-cell--clickable'
+        title='姓名'
+        align='center'
+        extra={user.name}
+        onClick={goto}
+        />
+        <Cell
+        className='nutui-cell--clickable'
+        title='电话'
+        align='center'
+        extra={user.phone}
+        onClick={goto}
+        />
+      </Cell.Group>
     </View>
   )
 }
