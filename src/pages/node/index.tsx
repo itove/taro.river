c57e60161193419005c330f1b97a01ba17facedf
Al Zee <z@alz.ee>
2023-10-31 20:29:08 +0800

    nth

--- src/pages/node/index.tsx
+++ src/pages/node/index.tsx
@@ -37,12 +37,8 @@ function Index() {
   return (
     <View className="nutui-react-demo">
       <View className="index">
-        欢迎使用 NutUI React 开发 Taro 多端项目。
       </View>
       <View className="index">
-        <Button type="primary" className="btn" onClick={goto}>
-          新增案件
-        </Button>
       </View>
     </View>
   )
