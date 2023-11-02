ff5d2413de3ab081a9853fefb8cdff14c3860f97
Al Zee <z@alz.ee>
2023-11-02 20:05:54 +0800

    node/index shows

--- src/pages/node/index.tsx
+++ src/pages/node/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View } from '@tarojs/components'
-import { Button } from "@nutui/nutui-react-taro"
+import { Button, Cell, Avatar, Row, Col } from "@nutui/nutui-react-taro"
+import { Right } from '@nutui/icons-react-taro'
 import Taro from '@tarojs/taro'
 import './index.scss'
 import { Env } from '../../env'
@@ -10,6 +11,7 @@ function goto() {
 }
 
 function Index() {
+  const [nodeList, setNodeList] = useState([])
   let uid: int
 
   useEffect(() => {
@@ -26,19 +28,31 @@ function Index() {
       })
       .then(res => {
         console.log(res)
+        const l = res.data.map(n => 
+          <Cell
+          className='nutui-cell--clickable'
+          title={n.title}
+          description={n.createdAt}
+          align='center'
+          extra={<><span>{n.type.name}</span> <Right className="ms-1" size="12" /></>}
+          />
+        )
+        console.log(l)
+        setNodeList(l)
       })
     })
     .catch(err => {
       console.log(err)
       Taro.redirectTo({url: '/pages/me/login'})
     })
-  })
+  }, [])
 
   return (
-    <View className="nutui-react-demo">
+    <View className="">
       <View className="index">
-      </View>
-      <View className="index">
+        <Cell.Group divider class="w-100">
+          {nodeList}
+        </Cell.Group>
       </View>
     </View>
   )
