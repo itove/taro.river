2a13a4ae7032e7a54ef19f00cb44ea7489ea8dd3
Al Zee <z@alz.ee>
2024-02-24 02:05:07 +0800

    nth

---
+++ src/compoents/irrigationForm.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useRef } from "react";
+import { View } from '@tarojs/components'
+import {
+  Swipe,
+  Cell,
+  Form,
+  Picker,
+	Uploader,
+  Button,
+  InputNumber,
+  Input,
+  TextArea,
+  Tabs
+} from '@nutui/nutui-react-taro'
+import { Right, Uploader as Plus } from '@nutui/icons-react-taro'
+import Taro from '@tarojs/taro'
+import { Env } from '../env'
+
+function IrrigationForm() {
+  return (
+    <>
+      <View className="label">土壤物理</View>
+      <Form.Item
+        label="日期"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+      <Form.Item
+        label="灌水量/m³/亩"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+    </>
+  )
+}
+
+export default IrrigationForm
