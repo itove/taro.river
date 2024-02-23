65f8fd321805534798c9ad6cf3eb71e92292f033
Al Zee <z@alz.ee>
2024-02-24 02:41:17 +0800

    nth

--- src/compoents/cost.tsx
+++ src/compoents/cost.tsx
@@ -10,18 +10,112 @@ import {
   InputNumber,
   Input,
   TextArea,
+  Collapse,
   Tabs
 } from '@nutui/nutui-react-taro'
 import { Right, Uploader as Plus } from '@nutui/icons-react-taro'
 import Taro from '@tarojs/taro'
 import { Env } from '../env'
+import CostForm from './costForm'
+import { ArrowDown } from '@nutui/icons-react-taro'
 
-function Cost() {
-  const [tabIndex, setTabIndex] = useState(0)
+function Tracking() {
   const [form] = Form.useForm()
 
+  const formSubmit = data => {
+    console.log(data)
+    let method = 'POST'
+    let url = Env.apiUrl + 'patterns'
+    let header = {}
+    if (!isNew) {
+      method = 'PATCH'
+      url = Env.apiUrl + 'patterns/' + pid
+      header = {
+        'content-type': 'application/merge-patch+json'
+      }
+    }
+    Taro.request({
+      method,
+      url,
+      header,
+      data
+    }).then((res) => {
+      if (res.statusCode === 200 || res.statusCode === 201) {
+        Taro.showToast({
+          title: '提交成功',
+          icon: 'success',
+          duration: 2000
+        }).then(() => {
+          Taro.reLaunch({ url: '/pages/node/index' })
+        })
+      } else {
+        Taro.showToast({
+          title: '系统错误',
+          icon: 'error',
+          duration: 2000
+        })
+        console.log('server error！' + res.errMsg)
+      }
+    })
+  }
+
   return (
-    <View><p>soil</p></View>
+    <Form
+      className="form"
+      form={form}
+      divider
+      // labelPosition="left"
+      onFinish={(values) => formSubmit(values)}
+      onFinishFailed={(values) => onFinishFailed(values)}
+      footer={
+        <Button formType="submit" block type="primary"> 保 存 </Button>
+      }
+    >
+      <Collapse defaultActiveName={['1']} expandIcon={<ArrowDown size="12"/>}>
+      <Collapse.Item title="高标准农田建设" name="1">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="平田整地" name="2">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="播种" name="3">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="灌溉排水" name="4">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="施肥1" name="5">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="施肥2" name="6">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="施肥3" name="7">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="管护1" name="8">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="管护2" name="9">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="管护3" name="10">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="产品1" name="11">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="产品2" name="12">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="产品3" name="13">
+        <CostForm />
+      </Collapse.Item>
+      <Collapse.Item title="产品4" name="14">
+        <CostForm />
+      </Collapse.Item>
+    </Collapse>
+    </Form>
   )
 }
 
---
+++ src/compoents/costForm.tsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect, useRef } from "react";
+import { View } from '@tarojs/components'
+import {
+  Swipe,
+  Cell,
+  Form,
+  Picker,
+	Uploader,
+  Button,
+  InputNumber,
+  Input,
+  TextArea,
+  Tabs
+} from '@nutui/nutui-react-taro'
+import { Right, Uploader as Plus } from '@nutui/icons-react-taro'
+import Taro from '@tarojs/taro'
+import { Env } from '../env'
+
+function TrackingForm() {
+  return (
+    <>
+      <Form.Item
+        label="内容"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+      <Form.Item
+        label="单位"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+      <Form.Item
+        label="总量"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+      <Form.Item
+        label="单价"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+      <Form.Item
+        label="总价"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+      <Form.Item
+        label="方法"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+      <Form.Item
+        label="名称"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+      <Form.Item
+        label="施用量"
+        name="name"
+        // initialValue={pattern.name}
+        rules={[
+          { max: 50, message: '不能超过50字符' },
+        ]}
+      >
+        <Input
+          className="nut-input-text"
+          type="text"
+        />
+      </Form.Item>
+    </>
+  )
+}
+
+export default TrackingForm
