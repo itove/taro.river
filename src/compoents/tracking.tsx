7682bc34ad2e94e4b6fe1629df2cbd44db109520
Al Zee <z@alz.ee>
2024-02-23 14:45:12 +0800

    nth

---
+++ src/compoents/tracking.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useRef } from "react";
+import { View } from '@tarojs/components'
+import {
+  Swipe,
+  Cell,
+  Form,
+  Picker,
+	Uploader,
+  Button,
+  InputNumber,
+  Input,
+  TextArea,
+  Tabs
+} from '@nutui/nutui-react-taro'
+import { Right, Uploader as Plus } from '@nutui/icons-react-taro'
+import Taro from '@tarojs/taro'
+import { Env } from '../env'
+
+function Tracking() {
+  const [tabIndex, setTabIndex] = useState(0)
+  const [form] = Form.useForm()
+
+  return (
+    <View><p>soil</p></View>
+  )
+}
+
+export default Tracking
